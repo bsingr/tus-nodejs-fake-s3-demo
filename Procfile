cb3d55bb68091babc86a3780c816496035ecff6b
Jens Bissinger <mail@jens-bissinger.de>
2018-10-24 14:03:12 +0200

    Replace s3rver with minio

---
+++ Procfile
@@ -0,0 +1,3 @@
+minio: docker run -p 9000:9000 --rm -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" minio/minio server /data
+tus: node tus.js
+serve: npx serve -l 5000 .
--- tus.js
+++ tus.js
@@ -1,20 +1,36 @@
 const tus = require('tus-node-server');
 
+process.on('unhandledRejection', (reason, p) => {
+    console.error(reason)
+})
+
 const server = new tus.Server();
 server.datastore = new tus.S3Store({
     path: '/files',
     bucket: 'my-bucket',
-    accessKeyId: 'access-key-id',
-    secretAccessKey: 'secret-access-key',
+    accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+    secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
     region: 'eu-west-1',
     partSize: 8 * 1024 * 1024, // each uploaded part will have ~8MB,
     tmpDirPrefix: 'tus-s3-store',
-    endpoint: 'http://127.0.0.1:4569/my-bucket/',
-    s3BucketEndpoint: true
+    signatureVersion: 'v2',
+    endpoint: 'http://127.0.0.1:9000/',
+    s3BucketEndpoint: true,
+    s3ForcePathStyle: true
 });
 
 const host = '127.0.0.1';
 const port = 1080;
 server.listen({ host, port }, () => {
     console.log(`[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`);
+});
+
+server.on(tus.EVENTS.EVENT_FILE_CREATED, (event) => {
+    console.log(`EVENT_FILE_CREATED ${JSON.stringify(event)}`);
+});
+server.on(tus.EVENTS.EVENT_ENDPOINT_CREATED, (event) => {
+    console.log(`EVENT_ENDPOINT_CREATED ${JSON.stringify(event)}`);
+});
+server.on(tus.EVENTS.EVENT_UPLOAD_COMPLETE, (event) => {
+    console.log(`EVENT_UPLOAD_COMPLETE ${JSON.stringify(event)}`);
 });
